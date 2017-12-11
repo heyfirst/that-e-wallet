1bb418b3d642807db171cdfd21aa7c0579cb2c48
Kanisorn S <firstziiz.k@gmail.com>
2017-12-11 15:03:49 +0700

    init post topup

--- src/main/java/com/tlt/thatewallet/servlet/TopUpServlet.java
+++ src/main/java/com/tlt/thatewallet/servlet/TopUpServlet.java
@@ -18,56 +18,49 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class TopUpServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        System.out.println("Hi");
-        getServletContext().getRequestDispatcher("/topup.jsp").forward(request, response);
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
+        response.setContentType("text/html;charset=UTF-8");
+        
+        // Render JSP
+        getServletContext()
+                .getRequestDispatcher("/topup.jsp")
+                .forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        // Get Parameter from POST Request
+        String topup = request.getParameter("topup");
+        String phoneNumber = request.getParameter("phoneNumber");
+        
+        // Check Existing User
+        User user = new User(phoneNumber);
+        if (user == null) {
+            // Render Error
+            return;
+        }
+        
+        
+        // Check Can Topup
+        
+        Double newBalance = user.getBalance() + topup;
+        if (newBalance <= 5000) {
+            // Render Confirm
+            // Render JSP
+            getServletContext()
+                .getRequestDispatcher("/confirm.jsp")
+                .forward(request, response);
+        } else {
+            // Render Error
+            return;
+        }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
         return "Short description";
